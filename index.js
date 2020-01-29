04c2443d5859cfce553c5163f910d165ea1aa5a2
Roberto Martinez <romama@gmail.com>
2020-01-29 11:51:30 -0500

    Replace archiver with cross-zip and get rid of rollup

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const fs = require('fs')
 const { join, extname } = require('path')
 const { tmpdir } = require('os')
-const archive = require('./archive')
-const { copyFile, hash } = require('./utils')
-const { TROPY, PLUGIN } = require('./constants')
+const zip = require('cross-zip')
+const { copyFile, hash } = require('./src/utils')
+const { TROPY, PLUGIN } = require('./src/constants')
 
 
 class Plugin {
@@ -98,7 +98,7 @@ class Plugin {
         this.writeJson()
       ])
 
-      const result = await archive(this.dir, output)
+      const result = await zip.zipSync(this.dir, output)
       logger.info(`${PLUGIN.NAME} wrote ${result.bytes} bytes to ${output}`)
 
       await this.rm(this.dir)
--- rollup.config.prod.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const dev = require('./rollup.config.dev')
-const uglify = require('rollup-plugin-uglify')
-
-
-module.exports = Object.assign({}, dev, {
-  plugins: dev.plugins.concat(uglify())
-})
